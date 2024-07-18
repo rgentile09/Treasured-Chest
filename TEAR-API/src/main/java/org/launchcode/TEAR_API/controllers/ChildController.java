81f0ab89cb303956ba6d7f7a0bda3c106d3fbee6
Ronnie Gentile <rgentile09@yahoo.com>
2024-07-18 13:02:08 -0500

    Child class/repository/controller added

---
+++ TEAR-API/src/main/java/org/launchcode/TEAR_API/controllers/ChildController.java
@@ -0,0 +1,31 @@
+package org.launchcode.TEAR_API.controllers;
+
+import org.launchcode.TEAR_API.models.Child;
+import org.launchcode.TEAR_API.repositories.ChildRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("")
+public class ChildController {
+    @Autowired
+    private ChildRepository childRepository;
+
+    @GetMapping
+    public List<Child> getAllChildren() {
+        return (List<Child>) childRepository.findAll();
+    }
+    @PostMapping("/add")
+    public Child addChild(@RequestParam String firstName, @RequestParam String birthDate, @RequestParam(required = false) String childPhoto) {
+        Child newChild = new Child();
+        newChild.setFirstName(firstName);
+        newChild.setBirthDate(birthDate);
+       if (childPhoto != null) {
+           newChild.setChildPhoto(childPhoto);
+       }
+        return childRepository.save(newChild);
+    }
+}
---
+++ TEAR-API/src/main/java/org/launchcode/TEAR_API/models/Child.java
@@ -0,0 +1,60 @@
+package org.launchcode.TEAR_API.models;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+
+import java.util.Date;
+@Entity
+public class Child {
+        @Id
+        @GeneratedValue
+        private Long id;
+
+        private String firstName;
+        private String birthDate;
+        private String childPhoto;
+
+    public Child() {
+    }
+
+    public Child(String firstName, String birthDate) {
+        this.firstName = firstName;
+        this.birthDate = birthDate;
+    }
+
+    public Child(String firstName, String birthDate, String childPhoto) {
+        this.firstName = firstName;
+        this.birthDate = birthDate;
+        this.childPhoto = childPhoto;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getBirthDate() {
+        return birthDate;
+    }
+
+    public void setBirthDate(String birthDate) {
+        this.birthDate = birthDate;
+    }
+
+    public String getChildPhoto() {
+        return childPhoto;
+    }
+
+    public void setChildPhoto(String childPhoto) {
+        this.childPhoto = childPhoto;
+    }
+}
