9c149777fc548051a5a59f7e97090239a2693e00
aakash-sachdev <aakashsachdev1992@gmail.com>
2024-07-23 21:10:19 -0500

    Database Setup

--- TEAR-API/src/main/java/org/launchcode/TEAR_API/data/MemoryRepository.java
+++ TEAR-API/src/main/java/org/launchcode/TEAR_API/data/MemoryRepository.java
@@ -5,5 +5,5 @@ import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface MemoryReposirtory extends CrudRepository<Memory, Long> {
+public interface MemoryRepository extends CrudRepository<Memory, Long> {
 }
