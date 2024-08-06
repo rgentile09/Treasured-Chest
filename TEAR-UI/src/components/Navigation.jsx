b37bee8f95e93df3e25a6fd85bc26d98cf457a8e
emilydion <edion468@gmail.com>
2024-08-06 18:34:23 -0500

    added bootstrap button

--- TEAR-UI/src/components/Navigation.jsx
+++ TEAR-UI/src/components/Navigation.jsx
@@ -11,8 +11,12 @@ const Navigation = () => {
                 </div>
             </a>
 
+            <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+                <span className="navbar-toggler-icon"></span>
+            </button>
+
             <div className="collapse navbar-collapse" id="navbarNavDropdown">
-                <ul className="navbar-nav">
+                <ul className="navbar-nav mr-auto mt-2 mt-lg-0">
                     <li className="nav-item">
                         <a className="nav-link" href="/login">Login</a>
                     </li>
@@ -28,11 +32,12 @@ const Navigation = () => {
                 </ul>
             </div>  
 
-            <div className="hamburger">
+            {/* <div className="hamburger">
                 <div className="bar"></div>
                 <div className="bar"></div>
                 <div className="bar"></div>
-            </div>
+            </div> */}
+        
         </nav>
         
     )
