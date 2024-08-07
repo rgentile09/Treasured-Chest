2fb6824569dd2f0083e128fced109bb80e7fb7f0
emilydion <edion468@gmail.com>
2024-08-07 16:33:53 -0500

    adding functioning nav with expanding capabilities

--- TEAR-UI/src/components/Navigation.jsx
+++ TEAR-UI/src/components/Navigation.jsx
@@ -4,19 +4,19 @@ const Navigation = () => {
     return (
         <nav className="navbar navbar-expand-lg navbar-dark">
             
-            <a className="navbar-brand" href="/">
+           <a className="navbar-brand" href="/">
                 <div className="logo-link">
                     <img className="logo" src="src/assets/treasured-chest-logo.svg"></img>
                     <h2>Treasured Chest</h2>
                 </div>
-            </a>
+             </a>
 
             <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
                 <span className="navbar-toggler-icon"></span>
             </button>
 
-            <div className="collapse navbar-collapse" id="navbarNavDropdown">
-                <ul className="navbar-nav mr-auto mt-2 mt-lg-0">
+           <div className="collapse navbar-collapse" id="navbarNavDropdown">
+               <ul className="navbar-nav mr-auto mt-2 mt-lg-0">
                     <li className="nav-item">
                         <a className="nav-link" href="/login">Login</a>
                     </li>
@@ -29,13 +29,13 @@ const Navigation = () => {
                     <li className="nav-item">
                         <a className="nav-link" href="/create-account">Create Account</a>
                     </li>
-                </ul>
-            </div>  
+             </ul>
+           </div>  
 
             {/* <div className="hamburger">
+               <div className="bar"></div>
                 <div className="bar"></div>
-                <div className="bar"></div>
-                <div className="bar"></div>
+                 <div className="bar"></div>
             </div> */}
         
         </nav>
